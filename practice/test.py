b172d7c0bdd5b8515d282eab9ab3c46d8c924717
Emmanuelkk223 <aemmanuelkakari@gmail.com>
2024-09-16 23:30:35 +0100

    test

--- practice/test.py
+++ practice/test.py
@@ -1,7 +1,7 @@
 due = 50
 while due > 0:
     coin = int(input("Insert: "))
-    if coin in [5, 10, 25]:
+    if coin in [5, 10, 25, 4]:
         due -= coin
         if due > 0:
             print(f"Amount Due: {due}")
